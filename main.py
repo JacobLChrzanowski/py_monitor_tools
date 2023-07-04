6c9e3a09fd7190cfb8202dde9974eb76161e6914
Jacob Chrzanowski <jlc3467@rit.edu>
2023-07-04 19:51:50 -0400

    Oh yeah, way better. We have bootstrapping now!

--- main.py
+++ main.py
@@ -2,11 +2,12 @@
 #
 #
 
-import errno, os, winreg
+import os, sys, errno, winreg, traceback
 import collections
 import code
 import math
 import re
+import logging
 from code import interact
 # proc_arch = os.environ['PROCESSOR_ARCHITECTURE'].lower()
 import VideoRegistryConfig as VRC
@@ -15,6 +16,9 @@ import Monitor as Monitor
 import pyreadline3
 import win32api
 import pywintypes
+
+logging.basicConfig(level = logging.DEBUG)
+LOG = logging.getLogger(__name__)
 
 def closeKeys(keys: list[winreg.HKEYType]):
     if type(keys) == winreg.HKEYType:
@@ -30,11 +34,12 @@ class Registry():
     enumValues_LoopLimit = 1000
     string_of_4_digits = re.compile(r"^\d{4}$")
     arch_keys = (winreg.KEY_WOW64_32KEY, winreg.KEY_WOW64_64KEY)
+    hives = {"HKLM":winreg.HKEY_LOCAL_MACHINE}
+
     def __init__(self, hivename: str):
         """
         """
-        self.hives = {"HKLM":winreg.HKEY_LOCAL_MACHINE}
-        self.openHive = winreg.ConnectRegistry(None, self.hives[hivename])
+        self.openHive = winreg.ConnectRegistry(None, Registry.hives[hivename])
         self.lastKey = None
         self.currentKey = None
 
@@ -87,58 +92,68 @@ class Registry():
                 break
         return outKeyNames
     # interact()
-# open_key_by_name(r"SYSTEM")
 
-
-# monitors = win32api.EnumDisplayMonitors()
-# monitor = Monitor.Monitor(monitors[0])
-# print(monitor)
 
 variables = globals().copy()
 variables.update(locals())
 shell = code.InteractiveConsole(variables)
 # shell.interact()
 
-# Registry testing
-# regObj = Registry("HKLM")
-# regObj.open_key_by_name(r"SYSTEM\CurrentControlSet\Control\Video")
-# regObj.open_key_by_name(r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall")
-# regObj.open_key_by_name(r'SYSTEM\CurrentControlSet\Control\Video\{21B7EC77-5DC6-11EB-BA75-34CFF6E5C29C}')
-# print(winreg.KEY_WOW64_32KEY)
-
-
-import winreg
-# print(r"*** Reading from %s ***" % aKey)
-print("Start")
-# test = winreg.OpenKey(aKey, "0000")
-# test2 = winreg.EnumKey(aKey,0)
-# print(test)
-# print(test2)
-
 
 class RegKey():
     """
     """
-    def __init__(self, parentKey: winreg.HKEYType, selfKeyName: str = None, name = None) -> 'RegKey':
+    hives = {"HKLM":(winreg.HKEY_LOCAL_MACHINE, 'HKEY_LOCAL_MACHINE')}
+
+    def __init__(self, parentRegKey: 'RegKey', selfKeyName: str = None, bootstrapKey: winreg.HKEYType = None) -> 'RegKey':
         """ pass selfKeyName to open that key under parentKey
         or pass None to have this key exist as the parentKey named as 'name'
         """
-        if selfKeyName:
-            # If we are opening a key under parentKey
-            self.parentKey: winreg.HKEYType = parentKey
-            self.name: str = selfKeyName
-            self.openKey()
+        if parentRegKey is None and bootstrapKey is not None:
+            # We are not being passed a RegKey instance
+            # We are in the process of bootstrpaping this object
+            self.key = bootstrapKey
+            self.name = selfKeyName
+            self.path = 'Computer\\' + self.name + '\\'
+        elif parentRegKey is not None and bootstrapKey is not None:
+            LOG.error('You cannot pass a parentRegKey and a boostrapKey to RegKey at the same time!')
+            raise Exception
         else:
-            # If we are instantiating this RegKey as the parent key
-            self.key = parentKey
-            self.name = name
-            self.open = True
+            # If we are opening a key under parentRegKey
+            # self.name :: This could do better as selfKeyName.split('\\')[-1] but then you would have to contend with refactoring openKey
+            self.parentKey: 'RegKey' = parentRegKey
+            self.name:      str = selfKeyName
+            self.path:      str = parentRegKey.path + '\\' + selfKeyName
+            self.openKey()
+        # else:
+        #     # If we are instantiating this RegKey as the already open parent key
+        #     self.key:           winreg.HKEYType = parentRegKey
+        #     self.name:          str = name
+        #     self.open:          bool = True
 
-        self.valuesEnumerated = False
-        self.valuesList = []
-        self.valuesODict = collections.OrderedDict()
+        self.valuesEnumerated:  bool = False
+        self.valuesList:        list['str'] = []
+        self.valuesODict:       collections.OrderedDict = collections.OrderedDict()
 
-        self.openSubKeys = []
+        self.openSubKeys:       list['RegKey'] = []
+
+    def ConnectHive(hivename: str):
+        """
+        """
+        new_OpenHive = winreg.ConnectRegistry(None, RegKey.hives[hivename][0])
+        new_OpenHiveName = RegKey.hives[hivename][1]
+        new_RegKey = RegKey(None, selfKeyName = new_OpenHiveName, bootstrapKey=new_OpenHive)
+        return new_RegKey
+    
+    def __str__(self):
+        outStr = ""
+        outStr += f'{self.name} :: {self.path}'
+        return outStr
+    
+    def __repr__(self):
+        outStr = ""
+        outStr += f'{self.name} :: {self.path}'
+        return outStr
 
     ### value Operations
     def enumValues(self):
@@ -166,23 +181,33 @@ class RegKey():
         self.openSubKeys.clear()
         self.enumSubKeyNames()
         for keyName in self.subKeyNames:
-            self.openSubKeys.append(RegKey(self.key, keyName))
+            self.openSubKeys.append(RegKey(self, keyName))
         return self.openSubKeys
-    def closeEnumSubKeys(self):
-        if self.openSubKeys:
-            for openSubKey in self.openSubKeys:
-                openSubKey.closeKey()
-            self.openSubKeys.clear()
-        else:
-            print(f"No child keys to close: {self.name}")
+    def closeSubKeys(self, closeSelf = False, deleteSubKeys = False):
+        for key in reversed(self.openSubKeys):
+            # We must iterate in reverse so we don't end up deleting just every other
+            # element due to i not matching list iteration correctly
+            key.closeSubKeys(closeSelf=True, deleteSubKeys=deleteSubKeys)
+            if deleteSubKeys:
+                # self.openSubKeys.remove(key)
+                del self.openSubKeys[self.openSubKeys.index(key)]
+        if closeSelf:
+            self.closeKey()
+    def openSubKey(self, subKeyName: str) -> 'RegKey':
+        newOpenSubKey = RegKey(self, subKeyName)
+        self.openSubKeys.append(newOpenSubKey)
+        return newOpenSubKey
 
     ### self Key Open/Close
     def openKey(self):
         try:
-            self.key = winreg.OpenKey(self.parentKey, self.name)
+            self.key = winreg.OpenKey(self.parentKey.key, self.name)
             self.open = True
         except Exception as e:
-            print(e)
+            LOG.error(e)
+            LOG.error(f"self.name = {self.name}")
+            # traceback.print_exception(*sys.exc_info())
+            raise e
             exit() #TODO: fix this
 
     def closeKey(self):
@@ -190,55 +215,71 @@ class RegKey():
             self.key.Close()
             self.open = False
         else:
-            print(f"{self.name} already closed")
+            LOG.warn(f"{self.name} already closed")
 
-baseRegKeyHKLM = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
-videoKey = winreg.OpenKey(baseRegKeyHKLM, r'SYSTEM\CurrentControlSet\Control\Video')
-VideoConfigKeyNames = Registry.enumSubKeyNames(videoKey)
-# print(f"enum VideoConfigKeyNames: {VideoConfigKeyNames}")
-for VideoConfigKeyName in VideoConfigKeyNames:
-    if VideoConfigKeyName != "{E0EC89FB-2A3A-11EB-BA6E-806E6F6E6963}":
-        continue
-    print(f"VideoConfigKeyName: {VideoConfigKeyName}")
-    open_driverConfigKey = winreg.OpenKey(videoKey, VideoConfigKeyName)
-    videoAdapters = Registry.enumSubKeyNames(open_driverConfigKey)
-    # print(f"enum Drivers: {videoAdapters}")
-    for videoAdapter in videoAdapters:
-        print(f"Driver: {videoAdapter}")
-        open_driverKey = winreg.OpenKey(open_driverConfigKey, videoAdapter)
-        if videoAdapter == "Video":
-            video_key_values = Registry.enumValues(open_driverKey)
-            print(f"video_key_values items: {video_key_values.items()}")
-        elif Registry.is_string_of_4_digits(videoAdapter):
-            #TODO go deeper and enum values
-            print('here')
-        else:
-            print("Found unexpected keyname under VideoConfigkey: {videoAdapter}")
-        temp = Registry.enumSubKeyNames(open_driverKey)
-        print(f"enum temp: {temp}")
-        open_driverKey.Close()
-    open_driverConfigKey.Close()
-print("BEGIN TESTING")
 
-videoKey = RegKey(videoKey, None, r"SYSTEM\CurrentControlSet\Control\Video")
-VideoConfigKeyNames = videoKey.openEnumSubKeys()
-for VideoConfigKey in VideoConfigKeyNames:
-    if VideoConfigKey.name != "{21B7EC77-5DC6-11EB-BA75-34CFF6E5C29C}":
+print("Start")
+open_HKLM = RegKey.ConnectHive('HKLM')
+print(open_HKLM)
+videoKey = open_HKLM.openSubKey(r'SYSTEM\CurrentControlSet\Control\Video')
+print(videoKey)
+a = videoKey.openEnumSubKeys()
+for i in a:
+    print(i)
+exit()
+baseRegKeyHKLM_raw = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
+# videoKey = winreg.OpenKey(KeyHKLM, r'SYSTEM\CurrentControlSet\Control\Video')
+baseRegKeyHKLM = RegKey(baseRegKeyHKLM_raw, None, r"HKLM\\")
+videoKey = baseRegKeyHKLM.openSubKey(r'SYSTEM\CurrentControlSet\Control\Video')
+print(baseRegKeyHKLM.name)
+print(videoKey.name)
+print('')
+print(winreg.QueryInfoKey(baseRegKeyHKLM.key))
+key_path = winreg.QueryPathEx(baseRegKeyHKLM.key, 6)
+winreg.Query
+exit()
+
+# graphicsDriverConfiguration = RegKey(videoKey, None, r"SYSTEM\CurrentControlSet\Control\GraphicsDrivers\Configuration")
+# graphicsDriverConfiguration = videoKey.openSubKey(r"Configuration")
+graphicsDriverConfiguration = videoKey.openEnumSubKeys()
+print(graphicsDriverConfiguration)
+print("\n\nEND")
+exit()
+gDCKeys = graphicsDriverConfiguration.openEnumSubKeys()
+for subKey in gDCKeys:
+    latestSubKey = ''
+    latestTime = 0
+    currentSubKeyValuesDict = subKey.getValuesODict()
+    if 'Timestamp' not in currentSubKeyValuesDict:
+        LOG.error(f"{subKey.name} did not have a 'Timestamp' value")
         continue
-    print(f"VideoConfigKeyName: {VideoConfigKey.name}")
-    videoAdapters = VideoConfigKey.openEnumSubKeys()
-    for videoAdapter in videoAdapters:
-        print(f"Driver: {videoAdapter.name}")
-        if videoAdapter.name == "Video":
-            video_key_values = videoAdapter.getValuesList()
-            print(f"video_key_values items: {video_key_values}")
-        elif Registry.is_string_of_4_digits(videoAdapter.name):
-            #TODO go deeper and enum values
-            key_values = videoAdapter.getValuesList()
-            for value in key_values:
-                print(value)
-        else:
-            print("Found unexpected keyname under VideoConfigkey: {videoAdapter}")
+    print(type(currentSubKeyValuesDict['Timestamp']))
+    print(currentSubKeyValuesDict['Timestamp'])
+
+# VideoConfigKeyNames = videoKey.openEnumSubKeys()
+# for VideoConfigKey in VideoConfigKeyNames:
+#     if VideoConfigKey.name != "{21B7EC77-5DC6-11EB-BA75-34CFF6E5C29C}":
+#         continue
+#     # print(f"VideoConfigKeyName: {VideoConfigKey.name}")
+#     videoAdapters = VideoConfigKey.openEnumSubKeys()
+#     for videoAdapter in videoAdapters:
+#         # print(f"Driver: {videoAdapter.name}")
+#         if videoAdapter.name == "Video":
+#             video_key_values = videoAdapter.getValuesList()
+#             # print(f"video_key_values items: {video_key_values}")
+#         elif Registry.is_string_of_4_digits(videoAdapter.name):
+#             #TODO go deeper and enum values
+#             key_values = videoAdapter.getValuesList()
+#             for value in key_values:
+#                 # print(value)
+#                 pass
+#         else:
+#             pass
+#             # print("Found unexpected keyname under VideoConfigkey: {videoAdapter}")
+
+LOG.info('Closing keys')
+graphicsDriverConfiguration.closeSubKeys(deleteSubKeys=True)
+print(graphicsDriverConfiguration.openSubKeys)
 
 videoKey.closeKey()
 # closeKeys(videoKey)
